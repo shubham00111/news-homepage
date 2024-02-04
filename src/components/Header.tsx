f041c69f7e5dc5cefa8fd4b2ef294ac80617a617
Shubham Rawat <shubhamb23efd@gmail.com>
2024-02-05 04:34:37 +0530

    challenge completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Logo from "../assets/images/logo.svg";
 import Hambergur from "../assets/images/icon-menu.svg";
 import CloseIcon from "../assets/images/icon-menu-close.svg";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+  const ref = useRef<HTMLElement>(null);
+
+  const handleMenu = () => {
+    if (ref.current !== null) {
+      if (!showMenu) {
+        ref.current.style.display = "block";
+      } else {
+        ref.current.style.display = "none";
+      }
+    }
+    setShowMenu(!showMenu);
+  };
   return (
     <header className="header">
+      <div ref={ref} id="overlay"></div>
+
       <div className="logo">
         <img src={Logo} alt="logo" />
       </div>
@@ -17,12 +32,21 @@ function Header() {
         <li>Trending</li>
         <li>Categories</li>
       </ul>
-      <div className="hamburger--icon">
-        <img className="hamburger--icon" src={Hambergur} />
+      <div className="hamburger--icon" onClick={handleMenu}>
+        <img src={Hambergur} />
       </div>
 
-      <div className="menu-modal">
-        <div className="close-icon">
+      <div
+        className="menu-modal"
+        style={{ display: showMenu ? "block" : "none" }}
+      >
+        <div
+          className="close-icon"
+          onClick={() => {
+            setShowMenu(false);
+            ref.current.style.display = "none";
+          }}
+        >
           <img src={CloseIcon} alt="" />
         </div>
         <ul>
